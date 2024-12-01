74ac0d8ed7b68f9232b796ba6fcdbe9cdddd57c2
AqFa-gh <54530136+AqFa-gh@users.noreply.github.com>
2024-12-01 12:24:51 +0700

    v2.1

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    const slides = document.querySelectorAll('.slide');
+    slides.forEach((slide) => (slide.style.display = 'none'));
+    slideIndex++;
+    if (slideIndex > slides.length) slideIndex = 1;
+    slides[slideIndex - 1].style.display = 'block';
+    setTimeout(showSlides, 5000); // Change slide every 5 seconds
+}
+
+function changeSlide(n) {
+    const slides = document.querySelectorAll('.slide');
+    slides.forEach((slide) => (slide.style.display = 'none'));
+    slideIndex += n;
+    if (slideIndex > slides.length) slideIndex = 1;
+    if (slideIndex < 1) slideIndex = slides.length;
+    slides[slideIndex - 1].style.display = 'block';
+}
